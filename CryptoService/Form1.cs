908ca3c74f350a6754dffd9d1ca96e9355cb497c
Maksim <mrmaksoracle89537089833@yandex.ru>
2023-12-08 17:24:50 +0300

    +

--- CryptoService/Form1.cs
+++ CryptoService/Form1.cs
@@ -40,17 +40,7 @@ namespace CryptoService
                     26, 8, 16, 7, 27, 20, 13, 2, 41, 52, 31, 37, 47, 55, 30, 40,
                     51, 45, 33, 48, 44, 49, 39, 56, 34, 53, 46, 42, 50, 36, 29, 32
                 };
-        List<byte> Etable = new List<byte>
-                {
-                    32, 1, 2, 3, 4, 5,
-                    4, 5, 6, 7, 8, 9,
-                    8, 9, 10, 11, 12, 13,
-                    12, 13, 14, 15, 16, 17,
-                    16, 17, 18, 19, 20, 21,
-                    20, 21, 22, 23, 24, 25,
-                    24, 25, 26, 27, 28, 29,
-                    28, 29, 30, 31, 32, 1
-                };
+
         List<List<byte>> S = new List<List<byte>>
             {
                  new List<byte>
@@ -131,7 +121,7 @@ namespace CryptoService
             var key = GenerateKey();
             keyRTB.Text = Convert.ToBase64String(key) + Environment.NewLine;
             //encrypt
-            var encrypted = _des_Crypt.Crypt(initTextRTB.Text, key, DES_Crypt.Mode.Cryptor);
+            var encrypted = _des_Crypt.Crypt(initTextRTB.Text, key, DES_Crypt.Mode.Encryptor);
             //set text
             ciphroTextRTB.Text = Convert.ToBase64String(encrypted.ToArray());
         }
--- CryptoService/Service/DES_Crypt.cs
+++ CryptoService/Service/DES_Crypt.cs
@@ -40,7 +40,7 @@ namespace CryptoService.Service
                     ),
                     ref N1, ref N2
                 );
-                feistel_cipher(mode, &N1, &N2, keys48b);
+                feistel_cipher(mode, ref N1, ref N2, keys48b);
                 split_64bits_to_8bits(
                     final_permutation(
                         join_32bits_to_64bits(N1, N2)
@@ -49,6 +49,57 @@ namespace CryptoService.Service
                 );
             }
 
+        }
+
+        private void feistel_cipher(Mode mode, ref uint N1, ref uint N2, ulong[] keys48b)
+        {
+            switch (mode)
+            {
+                case Mode.Encryptor:
+                    {
+                        for (byte round = 0; round < 16; ++round)
+                            round_feistel_cipher(ref N1, ref N2, keys48b[round]);
+                        swap(N1, N2);
+                        break;
+                    }
+                case Mode.Decryptor:
+                    {
+                        for (byte round = 15; round >= 0; --round)
+                            round_feistel_cipher(ref N1, ref N2, keys48b[round]);
+                        swap(N1, N2);
+                        break;
+                    }
+            }
+        }
+
+        private void round_feistel_cipher(ref uint N1, ref uint N2, ulong key48b)
+        {
+            uint temp = N2;
+            N2 = func_F(N2, key48b) ^ N1;
+            N1 = temp;
+        }
+
+        uint func_F(uint block32b, ulong key48b)
+        {
+            ulong block48b = expansion_permutation(block32b);
+            block48b ^= key48b;
+            block32b = substitutions(block48b);
+            return permutation(block32b);
+        }
+        uint substitutions(ulong block48b)
+        {
+            byte[] blocks4b, blocks6b = { 0 };
+            split_48bits_to_6bits(block48b, blocks6b);
+            substitution_6bits_to_4bits(blocks6b, blocks4b);
+            return join_4bits_to_32bits(blocks4b);
+        }
+
+        ulong expansion_permutation(uint block32b)
+        {
+            ulong block48b = 0;
+            for (byte i = 0; i < 48; ++i)
+                block48b |= (ulong)((block32b >> (32 - __EP[i])) & 0x01) << (63 - i);
+            return block48b;
         }
 
         private void split_64bits_to_8bits(ulong block64b, IEnumerable<byte> blocks8b)
@@ -149,17 +200,28 @@ namespace CryptoService.Service
                     57, 49, 41, 33, 25, 17, 9, 1,  59, 51, 43, 35, 27, 19, 11, 3,
                     61, 53, 45, 37, 29, 21, 13, 5, 63, 55, 47, 39, 31, 23, 15, 7
                 };
-        private static readonly byte[] __FP = 
+        private static readonly byte[] __FP =
                 {
                     40, 8, 48, 16, 56, 24, 64, 32, 39, 7, 47, 15, 55, 23, 63, 31,
                     38, 6, 46, 14, 54, 22, 62, 30, 37, 5, 45, 13, 53, 21, 61, 29,
                     36, 4, 44, 12, 52, 20, 60, 28, 35, 3, 43, 11, 51, 19, 59, 27,
                     34, 2, 42, 10, 50, 18, 58, 26, 33, 1, 41, 9 , 49, 17, 57, 25,
                 };
+        private static readonly byte[] __EP = 
+                {
+                    32, 1, 2, 3, 4, 5,
+                    4, 5, 6, 7, 8, 9,
+                    8, 9, 10, 11, 12, 13,
+                    12, 13, 14, 15, 16, 17,
+                    16, 17, 18, 19, 20, 21,
+                    20, 21, 22, 23, 24, 25,
+                    24, 25, 26, 27, 28, 29,
+                    28, 29, 30, 31, 32, 1
+                };
 
         public enum Mode : byte
         {
-            Cryptor,
+            Encryptor,
             Decryptor
         }
     }
